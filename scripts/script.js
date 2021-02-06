ba33b68be72eed5a83619bc2e5dedf6d21d26217
Evgeniy Pushkarev <joxan_p@mail.ru>
2021-02-07 01:46:23 +0700

    less 6

--- scripts/script.js
+++ scripts/script.js
@@ -3,7 +3,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
     let guess = 0;
 
-
     let isNamber = n => {
         return !isNaN(parseFloat(n)) && isFinite(n);
     };
@@ -13,27 +12,32 @@ document.addEventListener('DOMContentLoaded', () => {
         let number = Math.floor(Math.random()*(100 - 1) + 1);
         console.log('number1: ', number);
 
-        let divine = prompt('Угадай число от 1 до 100');
+        const rec = () => {
+            let divine = prompt('Угадай число от 1 до 100');
 
-        if(+divine > number){
-            alert("Загаданное число меньше");
-            divineNumber();
-        } else if (+divine < number && divine !== null) {
-            alert("Загаданное число больше");
-            divineNumber();
-        } else if (!isNamber(+divine)){
-            alert("Введи число");
-            divineNumber();
-        } else if(divine === null){
-            alert("Игра окончена");
-            guess = 1;
-        } else if (+divine === number){
-            alert("Поздравляю, Вы угадали!!!");
-            guess = 1;
+            if(+divine > number){
+                alert("Загаданное число меньше");
+                rec();
+            } else if (+divine < number && divine !== null) {
+                alert("Загаданное число больше");
+                rec();
+            } else if (!isNamber(+divine)){
+                alert("Введи число");
+                rec();
+            } else if(divine === null){
+                alert("Игра окончена");
+                guess = 1;
+            } else if (+divine === number){
+                alert("Поздравляю, Вы угадали!!!");
+                guess = 1;
+            }
+        };
+    
+        if (guess === 0){
+            rec();
         }
     };
 
-    if (guess === 0){
-        divineNumber();
-    }
+    divineNumber();
+
 });
